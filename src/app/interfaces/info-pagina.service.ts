58749b9ef022b89dd3907c6cddc0b559fb26f84d
Gonzalo Sian <gonzalosian@gmail.com>
2020-02-07 20:47:57 -0300

    Fin sección 4

---
+++ src/app/interfaces/info-pagina.service.ts
@@ -0,0 +1,16 @@
+// Interface generada de forma automatica con Ctrol+Shift+P
+// Buscar JSON a TS y convierte lo que tenemos en portapapeles.
+
+export interface InfoPagina {
+  titulo?: string;
+  email?: string;
+  nombre_corto?: string;
+  pagina_autor?: string;
+  facebook?: string;
+  twitter?: string;
+  instagram?: string;
+  linkedin?: string;
+  youtube?: string;
+  tumblr?: string;
+  equipo_trabajo?: any[];
+}
